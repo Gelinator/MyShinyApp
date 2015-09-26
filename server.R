ddccab85ef996b7815f33ab693a2ff2c618c4059
Gelinator <gelinas.dave@gmail.com>
2015-09-26 19:01:06 -0400

    add tabs, docs and links

--- server.R
+++ server.R
@@ -1,9 +1,26 @@
 require(shiny)
+require(caret)
+require(randomForest)
+require(manipulate)
+require(e1071)
+require(ggplot2)
+#loading data
+data("iris")
 
 shinyServer(
-        function(input,output){
-        output$oid1 <- renderPrint({input$id1})
-        output$oid2 <- renderPrint({input$id2})
-        output$odate <- renderPrint({input$date})
+        function(input,output) {
+                set.seed(543)
+                rf <- train(Species ~ Petal.Length+Petal.Width,iris,method="rf",ntree=500,"pca")
+                output$scatter <- renderPlot({
+                        plot <- qplot(Petal.Length,Petal.Width,data=iris,color=Species,geom="jitter")
+                        
+                        PL <- input$pl
+                        PW <- input$pw
+                        
+                        df <- data.frame(Petal.Length=PL,Petal.Width=PW)
+                        plot + geom_vline(xintercept=PL) + geom_hline(yintercept=PW) + annotate("text",x=2,y=2.5,label=paste("Prediction = ", predict(rf,df))) + annotate("text",x=2,y=2.4,label=paste("Petal Length = ", PL)) + annotate("text",x=2,y=2.3,label=paste("Petal Width = ", PW))
+                }
+                )
+                output$oAcc <- renderPrint({rf$results$Accuracy})
         }
 )
--- ui.R
+++ ui.R
@@ -1,29 +1,34 @@
 require(shiny)
 require(manipulate)
 require(caret)
+require(e1071)
+require(randomForest)
+require(ggplot2)
 
-shinyUI(pageWithSidebar(
-        headerPanel("Header"),
-        sidebarPanel(
-                h3('Sidebar text'),
-                numericInput("id1",'Numeric input',0,min=-100,max=100,step=10),
-                checkboxGroupInput("id2","Checkbox",
-                                   c("Value 1"="1",
-                                     "Value 2"="2",
-                                     "Value 3"="3")
+shinyUI(
+        pageWithSidebar(
+                headerPanel("Exploration and Prediction of the Iris Dataset"),
+                sidebarPanel(
+                        sliderInput('pl',"Petal Length",value=4,min = 1, max = 6.9,step=0.1),
+                        sliderInput('pw',"Petal Width",value=1.3,min = 0.1, max = 2.5,step=0.1),
+                        h3("How to make it work"),
+                        p("To use the iris exploration and prediction app, simply grab the sliders to select the petal length and petal width, and let the app predict what species this iris would be. It will also graphically point where it would stand compared to the real observations.")
                 ),
-                dateInput("date","Date:")
-        ),
-        mainPanel(
-                h3('Main Panel text'),
-                code(432/3),
-                p("text"),
-                h4("you entered"),
-                verbatimTextOutput("oid1"),
-                h4("you entered"),
-                verbatimTextOutput("oid2"),
-                h4("You dated"),
-                verbatimTextOutput("odate")
+                mainPanel(
+                        tabsetPanel(
+                                tabPanel("Prediction",plotOutput("scatter"),
+                                         h3("Predictive Model Accuracy"),
+                                         verbatimTextOutput("oAcc")),
+                                tabPanel("Documentation & Pitch", 
+                                         h4("The Mechanics"),
+                                         p("The predictions are provided thanks to a random forest model, built ontop of the iris dataset. Using the input from both sliders, it predicts the iris species from what it has learned from the dataset. As can be seen in the pitch, the petals are by far the most distinctive aspect of iris species."),
+                                         h4("The Pitch"),
+                                         p("http://rpubs.com/Gelinator/irisExploration"),
+                                         h4("The Code"),
+                                         p("https://github.com/Gelinator/MyShinyApp"),
+                                         p("Sorry, tried my best to make them clickable.")
+                                )
+                        )
+                )
         )
-)
 )
